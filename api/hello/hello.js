209a9f031c8c2d01c3cc54b1cee2f16da87dd04f
Mike Matos <mikematos84@gmail.com>
2019-12-13 08:30:51 -0500

    Updated serverless yaml

--- api/hello/hello.js
+++ api/hello/hello.js
@@ -1,12 +1,32 @@
 'use strict';
 
 const ServerlessUtils = require('../serverless-utils');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+AWS.config.update({ region: 'us-east-1' });
 
 module.exports.handler = async (event, context, callback) => {
-  const response = {
-    message: process.env.STAGE + ': This is a test, this is just a test',
-    input: event
+  try {
+    const response = await getItem();
+    return ServerlessUtils.Response(response);
+  } catch (err) {
+    return ServerlessUtils.Response(err);
+  }
+};
+
+const getItem = async () => {
+  const params = {
+    TableName: process.env.DEFAULT_TABLE,
+    Key: {
+      'USER_ID': 1
+    }
   }
 
-  return ServerlessUtils.Response(response);
-};
+  try {
+    const data = await docClient.get(params).promise();
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
